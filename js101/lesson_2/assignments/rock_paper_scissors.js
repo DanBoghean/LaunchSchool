102055fcb06971b40bb6ee55f97f9735e0e6dea9
Dan Boghean <danboghean@gmail.com>
2021-02-16 12:31:52 -0700

    RPS Assignment

--- js101/lesson_2/assignments/rock_paper_scissors.js
+++ js101/lesson_2/assignments/rock_paper_scissors.js
@@ -1,80 +1,145 @@
 const readline = require('readline-sync');
 const WINNING_COMBOS = {
-  rock: ['scissors', 'lizard'],
-  paper: ['rock', 'spock'],
-  scissors: ['paper', 'lizard'],
-  spock: ['scissors', 'rock'],
-  lizard: ['spock', 'paper']
+  rock: {scissors: 'crushes', lizard: 'crushes'},
+  paper: {rock: 'covers', spock: 'disproves'},
+  scissors: {paper: 'cuts', lizard: 'decapitates'},
+  spock: {scissors: 'smashes', rock: 'vaporizes'},
+  lizard: {spock: 'poisons', paper: 'eats'}
 };
+let score = {player: 0, computer: 0};
+let gamesPlayed = 0;
+let totalGames = 0;
 
 function prompt(message) {
   console.log(`=> ${message}`);
 }
 
 function playerWins(choice, computerChoice) {
-  return WINNING_COMBOS[choice].includes(computerChoice);
+  return Object.keys(WINNING_COMBOS[choice]).includes(computerChoice);
+}
+
+function updateScore(winner) {
+  score[winner] += 1;
+}
+
+function getWinnerMessage(choice, computerChoice) {
+  let message;
+  let playerWinner = playerWins(choice, computerChoice);
+
+  if (choice !== computerChoice) {
+    if (playerWinner) {
+      prompt(`${choice} ${WINNING_COMBOS[choice][computerChoice]} ${computerChoice}`);
+      message = 'You win!';
+      updateScore('player');
+    } else {
+      prompt(`${computerChoice} ${WINNING_COMBOS[computerChoice][choice]} ${choice}`);
+      message = 'Computer wins!';
+      updateScore('computer');
+    }
+  } else {
+    message = "It's a tie!";
+  }
+
+  return message;
 }
 
 function displayWinner(choice, computerChoice) {
-  prompt(`You chose ${choice}, the computer chose ${computerChoice}`);
+  prompt(`You chose ${choice}. The computer chose ${computerChoice}`);
+  let message = getWinnerMessage(choice, computerChoice);
+  prompt(`${message}`);
+}
 
-  let message;
-  if (choice === computerChoice) {
-    message = "It's a tie!";
-  } else {
-    message = playerWins(choice, computerChoice)
-      ? 'You win!'
-      : 'Computer wins';
+function displayScoreboard(currentScore) {
+  console.log('');
+  console.log(`------Current Score (Game ${gamesPlayed + 1} of ${totalGames})--------`);
+  console.log(`\tPlayer: ${currentScore['player']} Computer: ${currentScore[
+    'computer']}`);
+  console.log('-----------------------------------------');
+  console.log('\n');
+}
+
+function getRandomNumber(list) {
+  return Math.floor(Math.random() *
+                      list.length);
+}
+
+function isInvalidNumber(num) {
+  return num <= 0 || Number.isNaN(num);
+}
+
+function gamesToPlay() {
+  prompt('How many games do you want to play? ');
+  let answer = readline.question().toLowerCase();
+  while (isInvalidNumber(answer)) {
+    prompt('Please enter a number greater than 0');
+    answer = readline.question().toLowerCase();
   }
 
-  prompt(`${message}`);
+  return answer;
 }
 
 let options = Object.keys(WINNING_COMBOS);
 let shorthandOptions = {};
 let printOptions = [];
-options.forEach((x) => {
-  let firstChar = x[0];
+options.forEach((option) => {
+  let firstChar = option[0];
   if (!Object.keys(shorthandOptions).includes(firstChar)) {
-    shorthandOptions[firstChar] = x;
-    printOptions.push(`(${firstChar})${x.slice(1,)}`);
+    shorthandOptions[firstChar] = option;
+    printOptions.push(`(${firstChar})${option.slice(1,)}`);
   } else {
-    shorthandOptions[x.slice(0,2)] = x;
-    printOptions.push(`(${x.slice(0,2)})${x.slice(2,)}`);
+    shorthandOptions[option.slice(0,2)] = option;
+    printOptions.push(`(${option.slice(0,2)})${option.slice(2,)}`);
   }
 });
 
-while (true) {
+totalGames = gamesToPlay();
+console.clear();
+while (gamesPlayed < totalGames) {
   prompt(`Choose one, type the full word or the letter(s) in the parenthesis:
     ${printOptions.join(', ')}`);
   let choice = readline.question();
 
-  if (choice.length <= 2) {
-    while (!shorthandOptions[choice[0].toLowerCase()]) {
+  while (true) {
+    if (choice.length <= 2) {
+      if (shorthandOptions[choice.toLowerCase()]) {
+        choice = shorthandOptions[choice];
+        break;
+      }
       prompt("That's not a valid choice");
       choice = readline.question();
-    }
-
-    choice = shorthandOptions[choice];
-  } else {
-    while (!options.includes(choice)) {
+    } else {
+      if (options.includes(choice)) {
+        break;
+      }
       prompt("That's not a valid choice");
       choice = readline.question();
     }
   }
 
-  let randomIndex = Math.floor(Math.random() *
-                      options.length);
+  let randomIndex = getRandomNumber(options);
   let computerChoice = options[randomIndex];
 
+  console.clear()
   displayWinner(choice, computerChoice);
+  displayScoreboard(score);
 
-  prompt('Do you want to play again (y/n)?');
-  let answer = readline.question().toLowerCase();
-  while (answer[0] !== 'n' && answer[0] !== 'y') {
-    prompt('Please enter "y" or "n".');
-    answer = readline.question().toLowerCase();
+  let majorityWin = Math.floor(totalGames / 2);
+  if (score['player'] > majorityWin || score['computer'] > majorityWin) {
+    break;
   }
 
-  if (answer[0] !== 'y') break;
+  if (choice !== computerChoice) {
+    gamesPlayed += 1;
+  }
 }
+
+let playerScore = score['player'];
+let computerScore = score['computer'];
+let playerWinsAll = playerScore > computerScore;
+let winner = playerWinsAll ? `You won ${playerScore} out of ${totalGames} games.`
+  : `Computer won ${computerScore} out of ${totalGames} games.`;
+let winnnerMessage = playerWinsAll ? 'CONGRATULATIONS!' : "SORRY!";
+
+console.log(winnnerMessage);
+console.log(winner);
+console.log();
